179577a3ab16dc84c44444f97b2d518c25bbec1b
Luismi Feijoo <luism.feijoo26@gmail.com>
2022-02-26 01:41:36 +0100

    Update response

--- routes/index.js
+++ routes/index.js
@@ -16,31 +16,29 @@ router.post("/login", async (req, res) => {
   const { email } = req.body;
 
   const token = jwt.sign({ email }, process.env.JWTSECRET, {
-    expiresIn: 3600,
+    expiresIn: "2 days",
   });
 
   const html = loginTemplate({
     url: `${process.env.FRONT_URL}?accessToken=${token}`,
   });
-
+  
   const mailOptions = {
     from: process.env.MAILER_EMAIL_ID,
     to: email,
     subject: "Bienvenido a Hackithon Asobal App, sigue los pasos para iniciar sesión",
     html: html,
   };
-
+  
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      return res.status(500).json({ status: "mail not sent", errors: error });
+      return res.status(500).json({ status: "error", error: "something went wrong" });
     } else {
-      return res.json({
-        status:
-          "You will recieve an email with instructions to complete your registration!",
-        info: info,
-      });
+      return res.status(202).json();
     }
   });
 });
+
+
 
 module.exports = router;
